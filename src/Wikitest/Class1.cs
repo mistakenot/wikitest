a3138d1628d00855dffac7909499eeff53b71c2b
charlie <the_charlie_daly@hotmail.co.uk>
2018-03-09 01:13:30 +0700

    Working shit!

--- src/Wikitest/Class1.cs
+++ src/Wikitest/Class1.cs
@@ -1,8 +1,148 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WikiClientLibrary.Client;
+using WikiClientLibrary.Pages;
+using WikiClientLibrary.Sites;
+using WikiClientLibrary.Wikibase;
 
 namespace Wikitest
 {
     public class Class1
     {
+        public Entity GetEntity(string id)
+        {
+            WikiSite site = InitSite();
+
+            var entity = new Entity(site, id);
+            entity.RefreshAsync(EntityQueryOptions.FetchAllProperties).Wait();
+            return entity;
+        }
+
+        private static WikiSite InitSite()
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(LogLevel.Information, true);
+
+            var wikiClient = new WikiClient
+            {
+                // UA of Client Application. The UA of WikiClientLibrary will
+                // be append to the end of this when sending requests.
+                ClientUserAgent = "ConsoleTestApplication1/1.0",
+                Logger = loggerFactory.CreateLogger<WikiClient>(),
+            };
+
+            var site = new WikiSite(wikiClient, "https://www.wikidata.org/w/api.php")
+            {
+                Logger = loggerFactory.CreateLogger<WikiSite>()
+            };
+
+            // Waits for the WikiSite to initialize
+            site.Initialization.Wait();
+            return site;
+        }
+
+        public Task<IList<OpenSearchResultEntry>> Search(string term)
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(LogLevel.Information, true);
+
+            var wikiClient = new WikiClient
+            {
+                // UA of Client Application. The UA of WikiClientLibrary will
+                // be append to the end of this when sending requests.
+                ClientUserAgent = "ConsoleTestApplication1/1.0",
+                Logger = loggerFactory.CreateLogger<WikiClient>(),
+            };
+            
+            var uri = WikiSite.SearchApiEndpointAsync(wikiClient, "en.wikipedia.org").Result;
+            
+            var site = new WikiSite(wikiClient, uri);
+            
+            return site.OpenSearchAsync(term, 5);
+        }
+
+        public async Task<IEnumerable<string>> SearchPages(string term)
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(LogLevel.Information, true);
+
+            var wikiClient = new WikiClient
+            {
+                // UA of Client Application. The UA of WikiClientLibrary will
+                // be append to the end of this when sending requests.
+                ClientUserAgent = "ConsoleTestApplication1/1.0",
+                Logger = loggerFactory.CreateLogger<WikiClient>(),
+            };
+            
+            var uri = WikiSite.SearchApiEndpointAsync(wikiClient, "en.wikipedia.org").Result;
+            
+            var site = new WikiSite(wikiClient, uri);
+            
+            var results = await site.OpenSearchAsync(term, 5);
+
+            return results.Select(r => r.Title);
+        }
+
+        public async Task<IEnumerable<string>> SearchIdAsync(string term)
+        {
+            WikiSite site = await WikiDataSite();
+
+            var titles = new[] { "Mount Everest", "Test_(Unix)", "Inexistent title", "Earth" };
+
+            var enumerable = Entity.IdsFromSiteLinksAsync(site, "enwiki", new[] { "New York City" });
+
+            return await enumerable.ToList();
+        }
+
+        private static async Task<WikiSite> WikiDataSite()
+        {
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddConsole(LogLevel.Information, true);
+
+            var wikiClient = new WikiClient
+            {
+                // UA of Client Application. The UA of WikiClientLibrary will
+                // be append to the end of this when sending requests.
+                ClientUserAgent = "ConsoleTestApplication1/1.0",
+                Logger = loggerFactory.CreateLogger<WikiClient>(),
+            };
+
+            var site = new WikiSite(wikiClient, "https://www.wikidata.org/w/api.php")
+            {
+                Logger = loggerFactory.CreateLogger<WikiSite>()
+            };
+
+            await site.Initialization;
+            return site;
+        }
+
+        public Task<IEnumerable<Entity>> EntityFromTitle(string title) => EntityFromTitle(new string[] {title});
+
+        public async Task<IEnumerable<Entity>> EntityFromTitle(IEnumerable<string> titles)
+        {
+            var site = await WikiDataSite();
+            var ids = await Entity.IdsFromSiteLinksAsync(site, "enwiki", titles).ToList();
+
+            List<Entity> results = new List<Entity>();
+
+            foreach (var id in ids)
+            {
+                var entity = new Entity(site, id);
+                await entity.RefreshAsync(EntityQueryOptions.FetchAliases | EntityQueryOptions.FetchClaims);
+                results.Add(entity);
+
+            }
+            return results;
+        }
+
+        public async Task<IEnumerable<Entity>> EntityFromKeyword(string keywords)
+        {
+            var searchResults = await SearchPages(keywords);
+            var result = await EntityFromTitle(searchResults);
+            return result;
+        }
     }
 }
--- tests/Wikitest.Tests/UnitTest1.cs
+++ tests/Wikitest.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace Wikitest.Tests
@@ -8,7 +10,55 @@ namespace Wikitest.Tests
         [Fact]
         public void Test1()
         {
+            var entity = new Class1().GetEntity("Q513");
+            Assert.Equal("Mount Everest", entity.Labels["en"]);
+            Assert.Contains("Chumulangma", entity.Aliases["en"]);
+        }
 
+        [Fact]
+        public void SearchTest()
+        {
+            var result = new Class1().Search("San").Result;
+            
+            Assert.NotEmpty(result);
+            Assert.Equal("San", result.First().Title);
+            Assert.Equal("https://en.wikipedia.org/wiki/San", result.First().Url);
+        }
+
+        [Fact]
+        public void SearchIdsTests()
+        {
+            var result = new Class1().SearchIdAsync("New York City").Result;
+
+            Assert.NotEmpty(result);
+            Assert.Equal(new[] {"Q60"}, result);
+        }
+
+        [Fact]
+        public void GetEntityTest()
+        {
+            var result = new Class1().EntityFromTitle("New York City").Result.First();
+
+            Assert.Equal("Q60", result.Id);
+        }
+
+        [Fact]
+        public void TestSearch()
+        {
+            var result = new Class1().SearchPages("New York").Result;
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void EntityFromTermTest()
+        {
+            var result = new Class1().EntityFromKeyword("New York City").Result;
+            Assert.NotEmpty(result);
+            Assert.Equal(new string[]{"Q60", "Q328473", "Q7733", "Q13361030", "Q283207"}, result.Select(r => r.Id).ToArray());
+            Assert.NotEmpty(result.First().Aliases);
+            Assert.NotEmpty(result.First().Claims);
+            Assert.NotNull(result.First().Claims.FirstOrDefault(c => c.MainSnak.PropertyId == "P31"));
+            File.WriteAllText("q60", result.First().Claims.FirstOrDefault(c => c.MainSnak.PropertyId == "P31").MainSnak.RawDataValue.ToString());
         }
     }
 }
